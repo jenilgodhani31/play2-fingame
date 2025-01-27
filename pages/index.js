c85e9fc7f25abe818980c23de71300ff03edfb36
jenilgodhani31 <86819972+jenilgodhani31@users.noreply.github.com>
2025-01-27 17:36:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ export default function Homepage() {
         <link rel="icon" href="/gameImage/launch-icon.png" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4575195873243785"
           crossorigin="anonymous"></script>
-            <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-VWXQ4EGHNY"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
