d989ebd9d4f89ce7ea67cacd19a3ea857d07d504
jenilgodhani31 <86819972+jenilgodhani31@users.noreply.github.com>
2025-01-30 14:49:06 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,49 +1,49 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
-
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        {/* Google Tag (gtag.js) */}
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=AW-10944338984"
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: 
-              window.dataLayer = window.dataLayer || [];
-              function gtag() { dataLayer.push(arguments); }
-              gtag('js', new Date());
-              gtag('config', 'AW-10944338984');
-            ,
-          }}
-        />
-
-        {/* Google AdSense */}
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4575195873243785"
-          crossOrigin="anonymous"
-        />
-
-        <meta charSet="utf-8" />
-        <link rel="icon" href="/gameImage/launch-icon.png" />
-        <link rel="stylesheet" type="text/css" />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdn.jsdelivr.net/npm/neuicons/css/neu.min.css"
-        />
-      </Head>
-
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+import { Html, Head, Main, NextScript } from "next/document"; 
+import Script from "next/script"; 
+ 
+export default function Document() { 
+  return ( 
+    <Html lang="en"> 
+      <Head> 
+        {/* Google Tag (gtag.js) */} 
+        <Script 
+          strategy="afterInteractive" 
+          src="https://www.googletagmanager.com/gtag/js?id=AW-10944338984" 
+        /> 
+        <Script 
+          id="google-analytics" 
+          strategy="afterInteractive" 
+          dangerouslySetInnerHTML={{ 
+            __html: ` 
+              window.dataLayer = window.dataLayer || []; 
+              function gtag() { dataLayer.push(arguments); } 
+              gtag('js', new Date()); 
+              gtag('config', 'AW-10944338984'); 
+            `, 
+          }} 
+        /> 
+ 
+        {/* Google AdSense */} 
+        <Script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4575195873243785" 
+          crossOrigin="anonymous" 
+        /> 
+ 
+        <meta charSet="utf-8" /> 
+        <link rel="icon" href="/gameImage/launch-icon.png" /> 
+        <link rel="stylesheet" type="text/css" /> 
+        <link 
+          rel="stylesheet" 
+          type="text/css" 
+          href="https://cdn.jsdelivr.net/npm/neuicons/css/neu.min.css" 
+        /> 
+      </Head> 
+ 
+      <body> 
+        <Main /> 
+        <NextScript /> 
+      </body> 
+    </Html> 
+  ); 
 }
